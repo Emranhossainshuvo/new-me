cc7da1b577d9e174aa05dbb7f206759d0872e38d
Shuvo <shuvoorba28@gmail.com>
2023-12-02 10:07:39 +0600

    basic and responsive navbar created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
-    return (
-        <div>
-          <Outlet></Outlet>  
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
