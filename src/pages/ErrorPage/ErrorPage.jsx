a7b31b0f11064d748eaf73e043b9d6281f28a22a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-26 22:07:09 +0600

    added the basic error page to show error when user hit on any wrong url

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+    return (
+        <>
+            <div className="flex justify-center min-h-[100vh] items-center">
+                <h4 className="text-5xl block font-semibold">Something went wrong!!!</h4> 
+            </div>
+        </>
+    );
+};
+
+export default ErrorPage;
