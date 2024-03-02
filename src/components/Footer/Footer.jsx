71f0d13f7c4f7a50e0d5224807f6b136a75f2f41
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-02 18:52:03 +0600

    lower part of the footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
                 <div>
 
                 </div>
-                <div>
-                    Copyright &copy; {moment().format('YYYY')}
+                <div className='bg-[#000000] h-10 text-white flex justify-center items-center'>
+                    <p className='text-lg font-semibold'>Copyright &copy;{moment().format('YYYY')} all right reserved</p>
                 </div>
             </footer>
         </>
