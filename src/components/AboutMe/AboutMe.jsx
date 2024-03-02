76a401efc5c1c486e8a33ffd4044e0a62c24f818
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-02 20:44:24 +0600

    made about me page responsive and added some desing in thar component

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,16 +2,16 @@
 const AboutMe = () => {
     return (
         <>
-            <div className="flex flex-row-reverse justify-center items-center">
+            <div className="lg:flex flex-row-reverse justify-center items-center">
                 <div>
                     <img src="https://i.ibb.co/jRJwGT3/profile-pic.png" alt="" />
                 </div>
-                <div>
-                    <h2>About me</h2>
-                    <p>Hi,</p>
-                    <p>My name is Md. Emran Hossain</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sit sequi unde vitae error.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eveniet maiores facilis, voluptates nihil, voluptate a blanditiis, natus voluptatibus nesciunt sit placeat vero alias itaque!</p>
+                <div className="w-[50%]">
+                    <h2 className="bg-[#5D71AB] text-3xl font-semibold text-white p-2 w-[30%] rounded-e-full mb-3">About me</h2>
+                    <p className="pb-1 text-lg">Hi,</p>
+                    <p className="pb-1 text-lg">My name is Md. Emran Hossain</p>
+                    <p className="pb-1 text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sit sequi unde vitae error.</p>
+                    <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eveniet maiores facilis, voluptates nihil, voluptate a blanditiis, natus voluptatibus nesciunt sit placeat vero alias itaque!</p>
                 </div>
             </div>
         </>
