d5435b7a1eade25b83c94590ffbd4463a79e1ea5
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-02 20:37:28 +0600

    added the basic about me component

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,21 @@
+
+const AboutMe = () => {
+    return (
+        <>
+            <div className="flex flex-row-reverse justify-center items-center">
+                <div>
+                    <img src="https://i.ibb.co/jRJwGT3/profile-pic.png" alt="" />
+                </div>
+                <div>
+                    <h2>About me</h2>
+                    <p>Hi,</p>
+                    <p>My name is Md. Emran Hossain</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sit sequi unde vitae error.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eveniet maiores facilis, voluptates nihil, voluptate a blanditiis, natus voluptatibus nesciunt sit placeat vero alias itaque!</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AboutMe;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import AboutMe from "../../components/AboutMe/AboutMe";
 
 const Home = () => {
     return (
         <>
             <div>
+                <AboutMe></AboutMe>
                 <h4>Hi, this is from home</h4>
             </div>
         </>
