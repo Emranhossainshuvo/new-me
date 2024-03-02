3e6059f1041cb6c98fe4973c81c6b3cd80287d4d
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-02 18:28:28 +0600

    added some new dependencies

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
     return (
         <>
-            <div className="flex">
-                <div className="w-96 me-5">
-                    <div className="rounded-lg flex flex-col justify-center min-h-screen" style={{ position: 'sticky', top: '0', minHeight: '100vh' }}>
+            <div className="flex text-white">
+                <div className="lg:w-96 me-5">
+                    <div className="rounded-lg flex flex-col justify-center" style={{ position: 'sticky', top: '0', minHeight: '100vh' }}>
                         <img src="https://i.ibb.co/jRJwGT3/profile-pic.png" alt="" />
-                        <div>
-                            <h2>Md. Emran Hossain</h2>
-                            <p>Junior Mern stack develoer</p>
+                        <div className="text-start ps-6">
+                            <h2 className="text-2xl">Md. Emran Hossain</h2>
+                            <p className="text-xl text-[#8699A3]"> Mern stack develoer</p>
                             <p>+8801620366102</p>
                             <p>emran.shuv.dev@gmail.com</p>
                             <p>Dhaka, Bangladesh</p>
