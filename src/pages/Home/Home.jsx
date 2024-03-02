3aa2d368feddf89ce6e8329ff450df502eae6d18
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-02 20:48:23 +0600

    added the basic projects page and connected with the home page component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import AboutMe from "../../components/AboutMe/AboutMe";
+import Projects from "../../components/Projects/Projects";
 
 const Home = () => {
     return (
         <>
             <div>
                 <AboutMe></AboutMe>
+                <Projects></Projects>
                 <h4>Hi, this is from home</h4>
             </div>
         </>
