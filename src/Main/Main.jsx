33e809adff76dc0a42ee52b6111a1620210af1ac
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-02 18:37:25 +0600

    changed the whole layout of the website

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -3,23 +3,8 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
     return (
         <>
-            <div className="flex text-white">
-                <div className="lg:w-96 me-5">
-                    <div className="rounded-lg flex flex-col justify-center" style={{ position: 'sticky', top: '0', minHeight: '100vh' }}>
-                        <img src="https://i.ibb.co/jRJwGT3/profile-pic.png" alt="" />
-                        <div className="text-start ps-6">
-                            <h2 className="text-2xl">Md. Emran Hossain</h2>
-                            <p className="text-xl text-[#8699A3]"> Mern stack develoer</p>
-                            <p>+8801620366102</p>
-                            <p>emran.shuv.dev@gmail.com</p>
-                            <p>Dhaka, Bangladesh</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <Outlet></Outlet>
-                </div>
+            <div>
+                <Outlet></Outlet>
             </div>
         </>
     );
