f8585a58c9e65629c25424018d1892d83ce490ab
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-03 18:53:31 +0600

    added two projects and some texts

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,24 @@ const Projects = () => {
     return (
         <>
             <div>
-                
+                <div className="lg:flex justify-around items-center">
+                    <div className="w-[50%]">
+                        <img src="https://i.ibb.co/gFk6PBX/Screenshot-from-2024-03-02-21-46-13.png" alt="" />
+                    </div>
+                    <div className="w-[50%]">
+                        <h2 className="text-3xl font-semibold ">Online restaurant</h2>
+                        <p>Developed <span>Suicine</span>, an online restaurant shop. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores suscipit autem ratione harum ullam consectetur maiores, sunt amet provident laboriosam quisquam eius esse, accusantium ipsa? Magni iste ab explicabo sunt!  </p>
+                    </div>
+                </div>
+                <div className="lg:flex lg:flex-row-reverse justify-around items-center">
+                    <div className="w-[50%]">
+                        <img src="https://i.ibb.co/4YQqVLV/Screenshot-from-2024-03-02-21-43-54.png" alt="" />
+                    </div>
+                    <div className="w-[50%]">
+                        <h2 className="text-3xl font-semibold ">Car repaire shop</h2>
+                        <p>Developed <span>Car doctor</span>, an online car repairing shop. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores suscipit autem ratione harum ullam consectetur maiores, sunt amet provident laboriosam quisquam eius esse, accusantium ipsa? Magni iste ab explicabo sunt!  </p>
+                    </div>
+                </div>
             </div>
         </>
     );
