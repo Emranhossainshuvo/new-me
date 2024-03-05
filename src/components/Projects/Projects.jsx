e472f6e296f5c00ec0d9b6d58b96291aa334c215
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-05 18:43:49 +0600

    changed the desing layout for the projects cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,24 +8,41 @@ const Projects = () => {
                     <h3 className="text-5xl mb-5 text-center border-y-2 py-2 border-slate-700">Projects</h3>
                 </div>
 
-                <div className="lg:flex justify-around items-center">
-                    <div className="w-[50%]">
-                        <img src="https://i.ibb.co/gFk6PBX/Screenshot-from-2024-03-02-21-46-13.png" alt="" />
+
+                <div className="max-w-sm rounded-sm overflow-hidden shadow-lg">
+                    <img className="w-full" src="https://i.ibb.co/QbhL3gr/Screenshot-from-2024-03-05-18-39-20.png" alt="Sunset in the mountains" />
+                    <div className="px-6 py-4">
+                        <p>E-comers | Restaurant</p>
+                        <div className="font-bold text-xl mb-2">Suicine an online restaurant application</div>
+                        <p className="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolorum quaerat optio placeat, aliquid modi corporis ab exercitationem voluptate velit!
+                        </p>
                     </div>
-                    <div className="w-[50%]">
-                        <h2 className="text-3xl font-semibold ">Online restaurant</h2>
-                        <p>Developed <span>Suicine</span>, an online restaurant shop. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores suscipit autem ratione harum ullam consectetur maiores, sunt amet provident laboriosam quisquam eius esse, accusantium ipsa? Magni iste ab explicabo sunt!  </p>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">React</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Tailwind</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Node</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">MongoDB</span>
                     </div>
                 </div>
-                <div className="lg:flex lg:flex-row-reverse justify-around items-center">
-                    <div className="w-[50%]">
-                        <img src="https://i.ibb.co/4YQqVLV/Screenshot-from-2024-03-02-21-43-54.png" alt="" />
+
+                <div className="max-w-sm rounded-sm overflow-hidden shadow-lg">
+                    <img className="w-full" src="https://i.ibb.co/QbhL3gr/Screenshot-from-2024-03-05-18-39-20.png" alt="Sunset in the mountains" />
+                    <div className="px-6 py-4">
+                        <p>E-comers | Restaurant</p>
+                        <div className="font-bold text-xl mb-2">Suicine an online restaurant application</div>
+                        <p className="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolorum quaerat optio placeat, aliquid modi corporis ab exercitationem voluptate velit!
+                        </p>
                     </div>
-                    <div className="w-[50%]">
-                        <h2 className="text-3xl font-semibold ">Car repaire shop</h2>
-                        <p>Developed <span>Car doctor</span>, an online car repairing shop. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores suscipit autem ratione harum ullam consectetur maiores, sunt amet provident laboriosam quisquam eius esse, accusantium ipsa? Magni iste ab explicabo sunt!  </p>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">React</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Tailwind</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Node</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">MongoDB</span>
                     </div>
                 </div>
+
             </div>
         </>
     );
