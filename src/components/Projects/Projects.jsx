ecf45dde556eb5717f79f966ccd4368ed40bf477
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-03-07 20:53:47 +0600

    made the projects dynamic

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
                 <div className="max-w-sm my-4 mx-2 rounded-sm overflow-hidden shadow-lg hover:shadow-2xl delay-100">
                     <img className="w-full" src="https://i.ibb.co/XCgHYM9/Screenshot-from-2024-03-05-18-46-09.png" alt="Sunset in the mountains" />
                     <div className="px-2 py-4">
-                        <p>E-comers | Restaurant</p>
+                        <p>E-comers | Business app</p>
                         <div className="font-bold text-xl mb-2">Suicine an online restaurant application</div>
                         <p className="text-gray-700 text-base">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolorum quaerat optio placeat, aliquid modi corporis ab exercitationem voluptate velit!
@@ -43,17 +43,17 @@ const Projects = () => {
                 <div className="max-w-sm my-4 mx-2 rounded-sm overflow-hidden shadow-lg hover:shadow-2xl delay-100">
                     <img className="w-full" src="https://i.ibb.co/BqgwVX4/Screenshot-from-2024-03-06-21-34-15.png" alt="Sunset in the mountains" />
                     <div className="px-2 py-2">
-                        <p>E-comers | Restaurant</p>
-                        <div className="font-bold text-xl mb-2">Suicine an online restaurant application</div>
+                        <p>Donation | Online service</p>
+                        <div className="font-bold text-xl mb-2">An online platfor for donation</div>
                         <p className="text-gray-700 text-base">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolorum quaerat optio placeat, aliquid modi corporis ab exercitationem voluptate velit!
                         </p>
                     </div>
                     <div className="px-2 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">React</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">JavaScript</span>
                         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Tailwind</span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Node</span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">MongoDB</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Firebase</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">CSS</span>
                     </div>
                 </div>
 
